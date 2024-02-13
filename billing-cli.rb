15ba997b42208c236b87fd4c16ccbf95d70df2f3
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-02-13 09:52:56 +0700

    fetch from private repo

--- billing-cli.rb
+++ billing-cli.rb
@@ -1,14 +1,12 @@
 class BillingCli < Formula
   desc "CLI tools for our boys at Billing"
   homepage "https://github.com/bill-boys/billing-cli/"
+  head "https://github.com/bill-boys/billing-cli.git"
   version "0.0.1-alpha.1"
+  url "https://github.com/bill-boys/billing-cli/releases/download/v0.0.1-alpha.1/billing-cli.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "945f177a8dddfc29439bf228895e1be20ab7821e2727552b03a0b7344bb77837"
 
-  on_macos do
-    url "https://github.com/bill-boys/billing-cli/releases/download/v0.0.1-alpha.1/billing-cli.tar.gz"
-    sha256 "945f177a8dddfc29439bf228895e1be20ab7821e2727552b03a0b7344bb77837"
-
-    def install
-      bin.install "bill"
-    end
+  def install
+    bin.install "bill"
   end
 end
