bf1cd2f309652512c07e83b0b4a63500e5cef674
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-02-23 00:29:01 +0700

    update to v0.0.1-alpha.4

--- billing-cli.rb
+++ billing-cli.rb
@@ -2,9 +2,9 @@ class BillingCli < Formula
   desc "CLI tools for our boys at Billing"
   homepage "https://github.com/bill-boys/billing-cli-releases/"
   head "https://github.com/bill-boys/billing-cli-releases.git"
-  version "0.0.1-alpha.3"
-  url "https://github.com/bill-boys/billing-cli-releases/releases/download/v0.0.1-alpha.3/billing-cli.tar.gz"
-  sha256 "7755b127eabad1faed302c6342bbb902b07beaf617b910a820a1dbbe2a30349f"
+  version "0.0.1-alpha.4"
+  url "https://github.com/bill-boys/billing-cli-releases/releases/download/v0.0.1-alpha.4/billing-cli.tar.gz"
+  sha256 "4194e3f4d2f7884f758a019ae44b3f662857472bb170f414b9e02762d6f63676"
 
   def install
     bin.install "bill"
