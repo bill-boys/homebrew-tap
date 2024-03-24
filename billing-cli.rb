46c332a89f22d2b62cfce8c794aef568d2bacc36
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-03-25 00:46:40 +0700

    bump version beta 6

--- billing-cli.rb
+++ billing-cli.rb
@@ -2,9 +2,9 @@ class BillingCli < Formula
   desc "CLI tools for our boys at Billing"
   homepage "https://github.com/bill-boys/billing-cli-releases/"
   head "https://github.com/bill-boys/billing-cli-releases.git"
-  version "0.0.1-beta.5"
-  url "https://github.com/bill-boys/billing-cli-releases/releases/download/v0.0.1-beta.5/billing-cli.tar.gz"
-  sha256 "011017bfd3cbf32595ed45f2f9c8a279d5295d63fe018cdf5e168c5eb87e88c4"
+  version "0.0.1-beta.6"
+  url "https://github.com/bill-boys/billing-cli-releases/releases/download/v0.0.1-beta.6/billing-cli.tar.gz"
+  sha256 "468fa0883ee1f7de7a70500799d1be0516e02c4fc46fd04fad06a1144b7d257d"
 
   def install
     bin.install "bill"
