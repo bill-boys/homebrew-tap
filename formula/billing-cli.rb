bd33dc20243b482a7bdb5bfc6865d2fa00d409cd
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-11-03 09:08:26 +0700

    bump version beta 10

--- formula/billing-cli.rb
+++ formula/billing-cli.rb
@@ -2,9 +2,9 @@ class BillingCli < Formula
   desc "CLI tools for our boys at Billing"
   homepage "https://github.com/bill-boys/billing-cli-releases/"
   head "https://github.com/bill-boys/billing-cli-releases.git"
-  version "0.0.1-beta.9"
-  url "https://github.com/bill-boys/billing-cli-releases/releases/download/v0.0.1-beta.9/billing-cli.tar.gz"
-  sha256 "03f113af0b8f63195df856279128d81b3853d0013b7530beb99b43e00c99b1fb"
+  version "0.0.1-beta.10"
+  url "https://github.com/bill-boys/billing-cli-releases/releases/download/v0.0.1-beta.10/billing-cli.tar.gz"
+  sha256 "35cbbd84f914a5bcbc636f1b32430fa42a609125ee4557040bf5369142991cf1"
 
   def install
     bin.install "bill"
---
+++ formula/billing-cli@0.0.1-beta.10.rb
@@ -0,0 +1,12 @@
+class BillingCliAT001Beta10 < Formula
+  desc 'CLI tools for our boys at Billing'
+  homepage 'https://github.com/bill-boys/billing-cli-releases/'
+  head 'https://github.com/bill-boys/billing-cli-releases.git'
+  version '0.0.1-beta.10'
+  url 'https://github.com/bill-boys/billing-cli-releases/releases/download/v0.0.1-beta.10/billing-cli.tar.gz'
+  sha256 '35cbbd84f914a5bcbc636f1b32430fa42a609125ee4557040bf5369142991cf1'
+
+  def install
+    bin.install 'bill'
+  end
+end
